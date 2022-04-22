c1ce0a6bee04f68e8f23269bfcd5ded136a0cc56
= <=>
2022-04-22 19:16:11 +0000

    miner tweeks

--- src/components/CheatSheet/ResultTable.jsx
+++ src/components/CheatSheet/ResultTable.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { detailsUrl } from '../../powerShellCheatSheet.data'
-import { FaClone } from 'react-icons/fa'
+import { FaRegClone } from 'react-icons/fa'
 
 export default function ResultTable({ data, options, search }) {
   const [result, setResult] = useState([false])
@@ -27,36 +27,35 @@ export default function ResultTable({ data, options, search }) {
       alert("Copied to clipboard");
   });
   }
-  const onClickLink = id => {
-
-  }
   return (
     <>
-      <div className='px-4 font-medium'>There is {result[0] ? result.length : 0} result for " {search} ".</div>
-      <div className='w-full min-w-[600px] lg:max-w-[1100px] max-h-[65vh] font-semibold shadow-lg rounded-t-lg overflow-y-scroll'>
-        <div className='w-full grid grid-cols-12 gap-4 py-4 px-4 bg-sky-600 text-white text-base border-b-2 border-sky-800'>
+      <div className='px-4'>{search 
+        ? <div>There is {result[0] ? result.length : 0} result for " {search} ".</div>
+        : <div>There are "{result.length} PowerShell Commands"</div>}</div>
+      <div className='w-full min-w-[600px] lg:max-w-[1100px] max-h-[65vh] shadow-lg rounded-t-lg overflow-y-scroll'>
+        <div className='w-full grid grid-cols-12 gap-4 py-4 px-4 bg-sky-600 text-white text-base border-b-2 border-sky-800 font-semibold'>
           <div className='col-span-3'>Name</div>
-          <div className='col-span-2'>Cmdlet</div>
+          <div className='col-span-2'>Alias</div>
           <div className='col-span-5'>Description</div>
           <div className='col-span-2'>Type</div>
         </div>    
       {result.map((v, id) => (
         v ? <div key={id} className='--nth-child-bg w-full grid grid-cols-12 border-b-2 border-sky-800 gap-2 md:gap-4 py-4 px-2 md:px-6 text-gray-700 text-xs sm:text-sm lg:text-base'>
-              <div className='col-span-3 flex items-center space-x-2'>
-                <FaClone className='text-xl text-sky-800 hover:text-sky-500 cursor-pointer' onClick={() => onClickClone(id, 'name')} />
+              <div className='col-span-3 flex items-center space-x-2 font-bold text-sky-600'>
+                <FaRegClone className='text-xl text-sky-800 hover:text-sky-500 cursor-pointer' onClick={() => onClickClone(id, 'name')} />
                 <a href={detailsUrl + v.name.toLocaleLowerCase()} target='_blank'>{v.name}</a>
               </div>
               <div className='col-span-2 flex flex-col items-center space-x-2 space-y-1'>
                 {v.alias.map(v => <div className='w-full flex space-x-2'>
-                  <FaClone className='text-xl text-sky-700 hover:text-sky-500 cursor-pointer' onClick={() => onClickClone(id, 'alias')} />
+                  <FaRegClone className='text-xl text-sky-700 hover:text-sky-500 cursor-pointer' onClick={() => onClickClone(id, 'alias')} />
                   <div>{v}</div></div>)}
               </div>
               <div className='col-span-5 flex justify-start items-center'>{v.description}</div>
-              <div className={v.alias.length <= 0 ? 'flex justify-center items-center col-span-2 h-8 w-20 bg-orange-400 rounded-lg shadow-md' 
-                : 'col-span-2 flex justify-center items-center h-8 w-20 bg-blue-400 rounded-lg shadow-md'}>
+              <div className={v.alias.length <= 0 ? 'flex justify-center items-center col-span-2 h-8 w-20 bg-orange-400 rounded-lg shadow-md text-sm font-semibold' 
+                : 'col-span-2 flex justify-center items-center h-8 w-20 bg-blue-400 rounded-lg shadow-md text-sm font-semibold'}>
                   {v.alias.length <= 0 ? 'Function' : 'CmdLet'}</div>
             </div> 
-        : <div className='p-4 border-b-2 border-sky-700'>There is 0 result for " {search} ".</div>
+        : null
       ))}
       </div>
     </>
--- src/pages/CheatSheet.js
+++ src/pages/CheatSheet.js
@@ -1,7 +1,9 @@
 import { useState } from 'react'
+import { FaSearch } from 'react-icons/fa'
 import { data } from '../powerShellCheatSheet.data'
 import ResultTable from '../components/CheatSheet/ResultTable'
 import Options from '../components/CheatSheet/Options'
+import { isCompositeComponent } from 'react-dom/test-utils'
 
 
 export default function CheatSheet () {
@@ -15,8 +17,11 @@ export default function CheatSheet () {
     return (
         <div className='space-y-4 px-2 md:px-8'>
             <div className='flex justify-center space-x-4'>
-                <input type="search" placeholder='Search...' value={search} onChange={onChangeSearch}
-                className='rounded shadow-lg h-8 md:min-w-[600px] max-w-[1100px] mb-4 px-4 focus:outline-none ring-2 focus:ring-sky-500 hover:ring-sky-300 ring-sky-700' />
+                <div className='relative'>
+                    <FaSearch className='left-[.8rem] top-[.5rem] absolute opacity-40' />
+                    <input type="search" placeholder='Search...' value={search} onChange={onChangeSearch}
+                    className='rounded shadow-lg h-8 md:min-w-[600px] max-w-[1100px] mb-4 px-10 focus:outline-none ring-2 focus:ring-sky-500 hover:ring-sky-300 ring-sky-700' />
+                </div>
                 <div className=''><Options options={options} setOptions={setOptions} /></div>
             </div>
 
